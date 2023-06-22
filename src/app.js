9e4a136aa5ed233ca2e2633fec043e8ebf1316df
rmunguia16 <rmunguia16@comunidad.unam.mx>
2023-06-22 00:51:05 -0600

    finalizamos chat

--- src/app.js
+++ src/app.js
@@ -18,4 +18,19 @@ app.use('/', viewsRouter);
 
 const server = app.listen(3000, () => { console.log('Server is running on port 3000') });
 const io = new Server (server);
+const messages = [];
 
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado');
+
+    socket.on('message', (data) => {
+            messages.push(data);
+            io.emit('messageLogs', messages);
+    });
+
+    socket.on('authenticated', (user) => {
+        socket.emit('messageLogs', messages);
+        socket.broadcast.emit('newUserConnected', user);
+        console.log(user);
+    });
+});
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,55 @@
+// Objetivo: Archivo para el cliente del chat
+
 const socket = io();
+
+/* Swal.fire({
+    title: 'Bienvenido a la sala de chat',
+    text: 'Mensaje inicial',
+    icon: 'success'
+}); */
+
+let user;
+const chatbox = document.getElementById('chatbox');
+
+Swal.fire({
+    title: 'Identifiquese',
+    input: 'text',
+    text: 'Ingrese su nombre de usuario para ingresar en el chat',
+    inputValidator: (value) => {
+        return !value && 'Debe ingresar un nombre de usuario'
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+
+}).then((result) => {
+    user = result.value;
+    socket.emit('authenticated', user );
+});
+
+chatbox.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        if (chatbox.value.trim().length > 0) {
+            socket.emit('message', { user, message: chatbox.value });
+            chatbox.value = '';
+        }
+    }
+});
+
+socket.on('messageLogs', (data) => {
+    let log = document.getElementById('messageLogs');
+    let messages = '';
+    data.forEach (message => {
+        messages +=`${message.user}: ${message.message}<br>`;
+    });
+    log.innerHTML = messages;
+});
+
+socket.on('newUserConnected', (data) => {
+    Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmationButton: false,
+        timer: 3000,
+        title:`${data} se unió al chat`
+    });
+});
